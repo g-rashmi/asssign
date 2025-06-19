925fff0ea7f747a5f494630fdba58f174164bcf9
g-rashmi <rashmi.gupta7018@gmail.com>
2025-06-19 11:27:25 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { PrismaClient } = require("@prisma/client"); 
+const { PrismaClient } = require ("@prisma/client"); 
 
 const app = express();
 
