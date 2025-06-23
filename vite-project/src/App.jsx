13d333c519fb4d7dc5d9652c2b621a8da4535af5
g-rashmi <rashmi.gupta7018@gmail.com>
2025-06-23 14:31:33 +0530

    done

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,18 +1,30 @@
+import { useEffect, useState } from "react";
 import Login from "./components/Login";
  
 import Productlist from "./components/p";
 ``;
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
 function App() {
-  
+  const [user,setuser]=useState(null);
+  const [load,setload]=useState(true);
+  useEffect(()=>{ 
+const fn=onAuthStateChanged(auth,(u)=>{
+setuser(u);
+setload(false);
+return ()=>fn();
+})
+  },[])
+  if(load){<div>loading ... </div>}
   return (
     <div>
       <Router>
           
 
         <Routes>
-          <Route path="/" element={<Login />}></Route>
-          <Route path="/products" element={ <Productlist />}></Route>
+          <Route path="/" element={!user?<Login />:<Navigate to="/products"/>}></Route>
+          <Route path="/products" element={ user? <Productlist />:<Navigate to="/"/>}></Route>
         </Routes>``
       </Router>
     </div>
