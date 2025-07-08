8d5fe9d053c0fbf745ba76417931402db384cee2
g-rashmi <rashmi.gupta7018@gmail.com>
2025-07-08 14:48:51 +0530

    done

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,48 @@
 import { useEffect, useState } from "react";
 import Login from "./components/Login";
- 
 import Productlist from "./components/p";
-``;
-import { BrowserRouter as Router, Routes, Route,Navigate } from "react-router-dom";
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase";
-function App() {
-  const [user,setuser]=useState(null);
-  const [load,setload]=useState(true);
-  
-  useEffect(()=>{ 
-const fn=onAuthStateChanged(auth,(u)=>{
-setuser(u);
-setload(false);
-return ()=>fn();
-})
-  },[])
-  
-  if(load){<div>loading ... </div>}
-  return (
-    <div>
-      <Router>
-          
 
-        <Routes>
-          <Route path="/" element={!user?<Login />:<Navigate to="/products"/>}></Route>
-          <Route path="/products" element={ user? <Productlist />:<Navigate to="/"/>}></Route>
-        </Routes>``
-      </Router>
-    </div>
+function App() {
+  const [user, setUser] = useState(null);
+  const [load, setLoad] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (u) => {
+      setUser(u);
+      setLoad(false);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  if (load) {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <CircularProgress />
+    </Box>
+  );
+}
+
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={!user ? <Login /> : <Navigate to="/products" />} />
+        <Route path="/products" element={user ? <Productlist /> : <Navigate to="/" />} />
+      </Routes>
+    </Router>
   );
 }
 
