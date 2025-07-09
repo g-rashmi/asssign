26d3f19f41d2dd7143ba45a365431f06e004e5d8
g-rashmi <rashmi.gupta7018@gmail.com>
2025-07-09 12:56:27 +0530

    hii

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,15 +3,29 @@ import Login from "./components/Login";
 import Productlist from "./components/p";
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
-
+import { back_url } from "./b";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase";
-
+import {axios} from "axios"
 function App() {
   const [user, setUser] = useState(null);
   const [load, setLoad] = useState(true);
+const [serverWaking, setServerWaking] = useState(true);
 
+useEffect(() => {
+  const pingServer = async () => {
+    try {
+      await axios.get(`${back_url}/healthz`);
+    } catch (error) {
+      console.error("server is starting ..wait...");
+      console.log(error);
+    } finally {
+      setServerWaking(false);
+    }
+  };
+  pingServer();
+}, []);
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (u) => {
       setUser(u);
@@ -20,7 +34,7 @@ function App() {
     return () => unsubscribe();
   }, []);
 
-  if (load) {
+  if (load||serverWaking) {
   return (
     <Box
       sx={{
